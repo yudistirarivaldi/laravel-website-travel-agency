361bb9ee56310a68e44a6631e5556e63ca1c982f
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2023-01-28 22:01:32 +0800

    Templateing admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->namespace('Admin')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
